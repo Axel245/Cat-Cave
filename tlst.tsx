9eeb53898f713f13e57400cdcc0f4df91aa3e66c
Axel245 <angus-rox@live.com.au>
2016-05-24 21:27:26 +1000

    ready for jaryd

---
+++ tlst.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tlst" tilewidth="70" tileheight="70" spacing="2" margin="2" tilecount="196" columns="14">
+ <image source="../super-kewl-game/tileset.png" width="1024" height="1024"/>
+</tileset>
